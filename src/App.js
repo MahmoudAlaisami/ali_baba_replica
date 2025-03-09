b75e4bf03aef9accca2a4a7b00b4a010825549ad
MahmoudAlaisami <mahmoudalaissami@gmail.com>
2025-03-09 18:28:07 +0200

    first commit: basic react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Landing from './pages/Landing';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <HashRouter>
+        {/* <AuthProvider> */}
+          <Routes>
+            <Route element={<Landing />} path="/" />
+            {/* <Route element={<Login />} path="/login" /> */}
+            {/* <Route element={<ProtectedRoutes />}>
+              <Route element={<Home />} path="home" />
+            </Route> */}
+          </Routes>
+        {/* </AuthProvider> */}
+      </HashRouter>
     </div>
   );
 }
